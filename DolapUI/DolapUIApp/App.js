dfbe09015872e8f147c46f2d908800130daa50aa
Rvydkybs <rvyd.kybs@outlook.com>
2022-02-19 21:45:31 +0200

    giriş

--- DolapUI/DolapUIApp/App.js
+++ DolapUI/DolapUIApp/App.js
@@ -1,21 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View ,Button} from 'react-native';
+import { SafeAreaView } from 'react-native-web';
+import Myindex from './components';
 
+const myStyle= StyleSheet.create({
+  continer:{
+    flex:1,//iki divin ne kadar yer kapyalacağını belirlemek için
+  },
+  upper_conteiner:{
+    alignItems:'center',
+    padding:2,
+    backgroundColor:'green',
+    margin:10,//içerden boşluk bırakır
+    borderRadius:5,//kenar kıvrımı yumuşatma
+    marginTop:25,
+    flex:1,//yarı yarıya ekranı böl
+  },
+  bottom_container:{
+    padding:2,
+    backgroundColor:'blue',
+    margin:10,//içerden boşluk bırakır
+    borderRadius:5,//kenar kıvrımı yumuşatma
+    flex:1,
+  }
+});
 export default function App() {
-
+//görselliğe hitap eden yapılar components
+//görsel olmayan çalışan yapılar apiler
   return (
-    <View style={styles.container}>
-      <Text>HELLO RÜVEYDA.</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <View style={myStyle.upper_conteiner}>
+       <Text>HELLOW</Text>
+       <Text>SMYDCK</Text>
+        <Text>SMYDCK</Text>
+        <Text>SMYDCK</Text>
+        </View>
+        <View style={myStyle.bottom_container}>
+        <Button  title='Sumbit' onPress={()=>{console.log("is that work?")}}
+        color="pink"
+        ></Button>
+        </View>
+      <View style={myStyle.bottom_container}>
+        <Button title='Sumbit two' onPress={()=>{}}></Button>
+
+      </View>
+      </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
