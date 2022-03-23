054176b1050430c7abadb23b166bc842d5088d63
Rüveyda Kayabaşı <rvyd.kybs@outlook.com>
2022-03-23 12:33:52 +0200

    login simulation

--- ShopApp/src/Pages/Login/Login.js
+++ ShopApp/src/Pages/Login/Login.js
@@ -1,15 +1,31 @@
 import React from "react";
-import { View, Text, Image } from "react-native";
-import Button from "../../Components/Button/Button";
+import { View, Image } from "react-native";
 import styles from "./Login.style";
 import { Formik } from "formik";
+import { Alert } from "react-native";
+
+import usePost from "../../hooks/usePost/usePost";
 import Input from "../../Components/Input/Input";
+import Button from "../../Components/Button/Button";
 
-export default function Login() {
+export default function Login({ navigation }) {
+  const { data, loading, post, error } = usePost(); //hooks
+
   function handleLogin(values) {
-    console.log(values);
+    post("https://fakestoreapi.com/auth" + "/login", values);
   }
-
+  if (error) {
+    Alert.alert("Shop", "Something went wrong");
+  }
+  if (data) {
+    if (data.status === "Error") {
+      Alert.alert("Shop", "User not found");
+    } else {
+      navigation.navigate("ProductsPage");
+    }
+    console.log(data);
+  }
+  console.log(data);
   return (
     <View style={styles.container}>
       <View style={styles.logoContainer}>
@@ -25,13 +41,21 @@ export default function Login() {
               placeholder="enter user name"
               value={values.username}
               onType={handleChange("username")}
+              iconName="account"
             />
+
             <Input
               placeholder="enter your password"
               value={values.password}
               onType={handleChange("password")}
+              iconName="key"
+              isSecure
             />
-            <Button text="ENTER" onPress={handleSubmit}></Button>
+            <Button
+              text="LOGIN"
+              onPress={handleSubmit}
+              loading={loading}
+            ></Button>
           </View>
         )}
       </Formik>
