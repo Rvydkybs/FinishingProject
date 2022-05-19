c4135fb29313ddff7992100692222456927a33c3
Rüveyda Kayabaşı <rvyd.kybs@outlook.com>
2022-05-19 17:46:38 +0300

    update products page

--- ShopApp/src/Components/Error/Error.js
+++ ShopApp/src/Components/Error/Error.js
@@ -1,11 +1,19 @@
 import React from "react";
+import { View, Text } from "react-native";
 import AnimatedLottieView from "lottie-react-native";
 
 export default function Error() {
   return (
-    <AnimatedLottieView
-      source={require("../../assets/error.json")}
-      autoPlay
-    ></AnimatedLottieView>
+    <View
+      style={{
+        height: "100%",
+        width: "100%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Please try again:d</Text>
+    </View>
   );
 }
--- ShopApp/src/Pages/Products/Products.js
+++ ShopApp/src/Pages/Products/Products.js
@@ -8,17 +8,15 @@ import Loading from "../../Components/Loading/Loading";
 import Error from "../../Components/Error/Error";
 import styles from "./Products.style";
 import ActivityIndicator from "../../Components/Common/ActivityIndicator";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 //import useCategory from "../../hooks/usecategory/useCategory";
 
 const Stack = createStackNavigator();
 
 export default function Products({ navigation }) {
   const [isLoading, setIsLoading] = useState(true);
-  const { loading, data, error, setData } = useFetch(
-    //artık verileri custom hook ile çağırıyoruz
-    "https://fakestoreapi.com/products/"
-  );
+  const [data, setData] = useState();
+  const [error, setError] = useState(false);
 
   const handleProductSelect = (id) => {
     navigation.navigate("Detail", { id });
@@ -30,68 +28,84 @@ export default function Products({ navigation }) {
     ></ProductCard>
   );
 
+  const getData = async (url) => {
+    setIsLoading(true);
+    setError(false);
+    await axios
+      .get(url)
+      .then((response) => {
+        setData(response.data);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        setIsLoading(false);
+        setError(true);
+        console.log("hata", err);
+      });
+  };
+  useEffect(() => {
+    getData("https://fakestoreapi.com/products");
+  }, []);
+
   return (
     <View>
-      {loading ? (
+      <View style={styles.scrollView}>
+        <ScrollView
+          horizontal={true}
+          contentContainerStyle={{ height: 45 }}
+          showsHorizontalScrollIndicator={false}
+          //style={styles.scroll}
+        >
+          <Button
+            id="men"
+            onPress={() => getData("https://fakestoreapi.com/products")}
+            title="All products"
+            color="#00695C"
+          />
+          <Button
+            id="men"
+            onPress={() =>
+              getData(
+                "https://fakestoreapi.com/products/category/men's clothing"
+              )
+            }
+            title="men's clothing"
+            color="#00695C"
+          />
+          <Button
+            id="jewelery"
+            onPress={() =>
+              getData("https://fakestoreapi.com/products/category/jewelery")
+            }
+            title="jewelery"
+            color="#00695C"
+          />
+          <Button
+            id="electronics"
+            onPress={() =>
+              getData("https://fakestoreapi.com/products/category/electronics")
+            }
+            title="electronics"
+            color="#00695C"
+          />
+          <Button
+            id="women"
+            onPress={() =>
+              getData(
+                "https://fakestoreapi.com/products/category/women's clothing"
+              )
+            }
+            title="women's clothing"
+            color="#00695C"
+          />
+        </ScrollView>
+      </View>
+      {isLoading ? (
         <ActivityIndicator />
       ) : error ? (
         <Error />
       ) : (
         <View>
-          <View style={styles.scrollView}>
-            <ScrollView
-              horizontal={true}
-              contentContainerStyle={{ height: 45 }}
-              showsHorizontalScrollIndicator={false}
-              //style={styles.scroll}
-            >
-              <Button
-                id="men"
-                onPress={async () => {
-                  const { data: responseData } = await axios.get(
-                    "https://fakestoreapi.com/products/category/men's clothing"
-                  );
-                  setData(responseData);
-                }}
-                title="men's clothing"
-                color="#00695C"
-              />
-              <Button
-                id="jewelery"
-                onPress={async () => {
-                  const { data: responseData } = await axios.get(
-                    "https://fakestoreapi.com/products/category/jewelery"
-                  );
-
-                  setData(responseData);
-                }}
-                title="jewelery"
-                color="#00695C"
-              />
-              <Button
-                id="electronics"
-                onPress={async () => {
-                  const { data: responseData } = await axios.get(
-                    "https://fakestoreapi.com/products/category/electronics"
-                  );
-                  setData(responseData);
-                }}
-                title="electronics"
-                color="#00695C"
-              />
-              <Button
-                id="women"
-                onPress={async () => {
-                  const { data: responseData } = await axios.get(
-                    "https://fakestoreapi.com/products/category/women's clothing"
-                  );
-                  setData(responseData);
-                }}
-                title="women's clothing"
-                color="#00695C"
-              />
-            </ScrollView>
-          </View>
           <FlatList
             data={data}
             renderItem={renderProduct}
